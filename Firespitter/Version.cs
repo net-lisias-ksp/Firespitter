60718132bc6ab193181af53cafa0be498527438d
Lisias T <me@lisias.net>
2018-06-15 08:09:25 -0300

    Normalizing Versioning

---
+++ Firespitter/Version.cs
@@ -0,0 +1,12 @@
+﻿namespace Firespitter
+{
+	static class Version
+	{
+		public const int major = 7;
+		public const int minor = 9;
+		public const int patch = 0;
+		public const int build = 0;
+		public const string Number = "7.9.0.0";
+		public const string Text = Number + " Unofficial";
+	}
+}
