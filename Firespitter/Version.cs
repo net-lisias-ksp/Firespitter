072d79062ef465d172c44d93713a5ff7b471d58c
Lisias T <me@lisias.net>
2018-07-27 01:18:32 -0300

    Bumping Version

--- Firespitter/Version.cs
+++ Firespitter/Version.cs
@@ -5,8 +5,8 @@
 		public const int major = 7;
 		public const int minor = 9;
 		public const int patch = 0;
-		public const int build = 0;
-		public const string Number = "7.9.0.0";
+		public const int build = 1;
+		public const string Number = "7.9.0.1";
 		public const string Text = Number + " Unofficial";
 	}
 }
